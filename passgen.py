58408d7c93abf016dbce44617bc338a1333d6939
alka <bilgi.alikaya@gmail.com>
2016-10-25 21:43:43 +0300

    first commit

---
+++ passgen.py
@@ -0,0 +1,39 @@
+import random
+
+soru = int(raw_input("kac karakterli parola istiyorsun"))
+Bharf = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+Kharf = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+Sayi = ["0","1","2","3","4","5","6","7","8","9"]
+OzelKarakter = [".",",",":",";","+","-","%","&","/","(",")","=","?","-"]
+
+sifre = ""
+
+while soru >= 0:
+    if len(sifre)>=soru:
+        break
+    else:
+        karekterSecenSayi = random.randint(1,25)
+        sifre += Bharf[karekterSecenSayi]
+
+    if len(sifre)>=soru:
+        break
+    else:
+        karekterSecenSayi = random.randint(1,25)
+        sifre += Kharf[karekterSecenSayi]
+
+    if len(sifre)>=soru:
+        break
+    else:
+        karekterSecenSayi = random.randint(1,9)
+        sifre += Sayi[karekterSecenSayi]
+    if len(sifre)>=soru:
+        break
+    else:
+        karekterSecenSayi = random.randint(1,13)
+        sifre += OzelKarakter[karekterSecenSayi]
+
+
+
+
+
+print "sifreniz:", sifre
