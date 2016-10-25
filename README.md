5ab32dc70de482671039881a075267c5ec25340c
alka <bilgi.alikaya@gmail.com>
2016-10-25 22:05:29 +0300

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # passgen
 Büyük küçük harf ve özel karekterleri karıştırarak güçlü bir parola oluşturur.
+
+Python 2.7.12 kullanılarak oluşturuldu.
--- passgen.py
+++ passgen.py
@@ -1,6 +1,6 @@
 import random
 
-soru = int(raw_input("kac karakterli parola istiyorsun"))
+soru = int(raw_input("kac karakterli parola istiyorsun\n"))
 Bharf = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 Kharf = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 Sayi = ["0","1","2","3","4","5","6","7","8","9"]
